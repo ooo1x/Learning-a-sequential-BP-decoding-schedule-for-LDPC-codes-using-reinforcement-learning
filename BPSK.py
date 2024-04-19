95c292967ecef7b109bc3816da050f658433f273
YixuanWu <wuyixuan_2008@icloud.com>
2024-04-19 18:02:38 +0200

    BPSK transmission system

--- BPSK.py
+++ BPSK.py
@@ -1,182 +1,74 @@
 import numpy as np
-from math import pi
 import matplotlib.pyplot as plt
 import scipy.signal as signal
-import math
+from math import pi, floor
 
-#码元数
+# Parameters
+size = 20  # Number of symbols
+sampling_t = 0.01  # Sampling interval
+fc = 4000  # Carrier frequency
+fs = 40 * fc  # Sampling frequency
 
-size = 10
-
-sampling_t = 0.01
-
+# Time vectors
 t = np.arange(0, size, sampling_t)
-
-
-
-# 随机生成信号序列
-
-a = np.random.randint(0, 2, size)
-
-m = np.zeros(len(t), dtype=np.float32)
-
-for i in range(len(t)):
-
-    m[i] = a[math.floor(t[i])]
-
-fig = plt.figure()
-
-ax1 = fig.add_subplot(3, 1, 1)
-
-
-fc = 4000
-
-fs = 20 * fc # 采样频率
-
 ts = np.arange(0, (100 * size) / fs, 1 / fs)
 
-coherent_carrier = np.cos(np.dot(2 * pi * fc, ts))
+# Generate random binary signal sequence
+a = np.random.randint(0, 2, size)
+m = np.zeros(len(t), dtype=np.float32)
+for i in range(len(t)):
+    m[i] = a[floor(t[i])]
 
+# Modulation
+coherent_carrier = np.cos(2 * pi * fc * ts)
+bpsk = np.cos(2 * pi * fc * ts + pi * (m - 1) + pi / 4)
 
-
-bpsk = np.cos(np.dot(2 * pi * fc, ts) + pi * (m - 1) + pi / 4)
-
-
-
-# BPSK调制信号波形
-
-ax2 = fig.add_subplot(3, 1, 2)
-
-ax2.set_title('BPSK调制信号', fontsize=20)
-
-plt.axis([0,size,-1.5, 1.5])
-
-plt.plot(t, bpsk, 'r')
-
-
-
-
-
-# 定义加性高斯白噪声
-
+# Additive White Gaussian Noise (AWGN)
 def awgn(y, snr):
-
     snr = 10 ** (snr / 10.0)
-
     xpower = np.sum(y ** 2) / len(y)
-
     npower = xpower / snr
-
     return np.random.randn(len(y)) * np.sqrt(npower) + y
-
-
-
-# 加AWGN噪声
 
 noise_bpsk = awgn(bpsk, 5)
 
-
-
-# BPSK调制信号叠加噪声波形
-
-ax3 = fig.add_subplot(3, 1, 3)
-
-ax3.set_title('BPSK调制信号叠加噪声波形', fontsize = 20)
-
-plt.axis([0, size, -1.5, 1.5])
-
-plt.plot(t, noise_bpsk, 'r')
-
-
-
-
-
-# 带通椭圆滤波器设计，通带为[2000，6000]
-
-[b11,a11] = signal.ellip(5, 0.5, 60, [2000 * 2 / 80000, 6000 * 2 / 80000], btype = 'bandpass', analog = False, output = 'ba')
-
-
-
-# 低通滤波器设计，通带截止频率为2000Hz
-
-[b12,a12] = signal.ellip(5, 0.5, 60, (2000 * 2 / 80000), btype = 'lowpass', analog = False, output = 'ba')
-
-
-
-# 通过带通滤波器滤除带外噪声
-
+# Filtering
+b11, a11 = signal.ellip(5, 0.5, 60, [2000 * 2 / fs, 6000 * 2 / fs], btype='bandpass', analog=False, output='ba')
+b12, a12 = signal.ellip(5, 0.5, 60, 2000 * 2 / fs, btype='lowpass', analog=False, output='ba')
 bandpass_out = signal.filtfilt(b11, a11, noise_bpsk)
-
-
-
-# 相干解调,乘以同频同相的相干载波
-
 coherent_demod = bandpass_out * (coherent_carrier * 2)
-
-
-
-# 通过低通滤波器
-
 lowpass_out = signal.filtfilt(b12, a12, coherent_demod)
 
-fig2 = plt.figure()
-
-bx1 = fig2.add_subplot(3, 1, 1)
-
-bx1.set_title('本地载波下变频，经低通滤波器后', fontsize=20)
-
-plt.axis([0, size, -1.5, 1.5])
-
-plt.plot(t, lowpass_out, 'r')
-
-
-
-#抽样判决
-
+# Sampling and decision making
 detection_bpsk = np.zeros(len(t), dtype=np.float32)
-
 flag = np.zeros(size, dtype=np.float32)
-
-
-
-for i in range(10):
-
-    tempF = 0
-
-    for j in range(100):
-
-        tempF = tempF + lowpass_out[i * 100 + j]
-
-    if tempF > 0:
-
-        flag[i] = 1
-
-    else:
-
-        flag[i] = 0
-
 for i in range(size):
+    tempF = np.sum(lowpass_out[i * 100: (i + 1) * 100])
+    flag[i] = 1 if tempF > 0 else 0
+    detection_bpsk[i * 100: (i + 1) * 100] = flag[i]
 
-    if flag[i] == 0:
+# Plot setup
+fig, axs = plt.subplots(3, 1, figsize=(10, 15))
+axs[0].plot(t, m)
+axs[0].set_title('Generate N binary bits signal', fontsize=20)
+axs[0].axis([0, size, -0.5, 1.5])
 
-        for j in range(100):
+axs[1].plot(t, bpsk, 'r')
+axs[1].set_title('BPSK modulated signal',  fontsize=20)
+axs[1].axis([0, size, -1.5, 1.5])
 
-            detection_bpsk[i * 100 + j] = 0
+axs[2].plot(t, noise_bpsk, 'r')
+axs[2].set_title('BPSK modulated signal + Noise', fontsize=20)
+axs[2].axis([0, size, -1.5, 1.5])
 
-    else:
+fig2, bx = plt.subplots(2, 1, figsize=(10, 15))
 
-        for j in range(100):
+bx[0].plot(t, lowpass_out)
+bx[0].set_title('After low pass filtered signal', fontsize=20)
+bx[0].axis([0, size, -1.5, 1.5])
 
-            detection_bpsk[i * 100 + j] = 1
-
-
-
-bx2 = fig2.add_subplot(3, 1, 2)
-
-bx2.set_title('BPSK信号抽样判决后的信号')
-
-plt.axis([0, size, -0.5, 1.5])
-
-plt.plot(t, detection_bpsk, 'r')
-
+bx[1].plot(t, detection_bpsk)
+bx[1].set_title('BPSK signal after sampling and decision', fontsize=20)
+bx[1].axis([0, size, -0.5, 1.5])
+plt.tight_layout()
 plt.show()
