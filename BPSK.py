a67847927aad3056a5fbbde0e471ad55df9d4ace
Yixuan Wu <161979224+ooo1x@users.noreply.github.com>
2024-04-29 15:32:51 +0200

    BPSK transmission system

--- BPSK.py
+++ BPSK.py
@@ -3,49 +3,57 @@ import matplotlib.pyplot as plt
 import scipy.signal as signal
 from math import pi, floor
 
-# Parameters
-size = 20  # Number of symbols
-sampling_t = 0.01  # Sampling interval
-fc = 4000  # Carrier frequency
-fs = 40 * fc  # Sampling frequency
+def generate_binary_sequence(size, sampling_t):
+    t = np.arange(0, size, sampling_t)
+    a = np.random.randint(0, 2, size)
+    m = np.zeros(len(t), dtype=np.float32)
+    for i in range(len(t)):
+        m[i] = a[floor(t[i])]
+    return t, m
 
-# Time vectors
-t = np.arange(0, size, sampling_t)
-ts = np.arange(0, (100 * size) / fs, 1 / fs)
+def modulate_signal(t, m, fc, fs):
+    ts = np.arange(0, (100 * len(m)) / fs, 1 / fs)
+    coherent_carrier = np.cos(2 * pi * fc * ts)
+    bpsk = np.cos(2 * pi * fc * ts + pi * (m - 1) + pi / 4)
+    return bpsk, coherent_carrier
 
-# Generate random binary signal sequence
-a = np.random.randint(0, 2, size)
-m = np.zeros(len(t), dtype=np.float32)
-for i in range(len(t)):
-    m[i] = a[floor(t[i])]
-
-# Modulation
-coherent_carrier = np.cos(2 * pi * fc * ts)
-bpsk = np.cos(2 * pi * fc * ts + pi * (m - 1) + pi / 4)
-
-# Additive White Gaussian Noise (AWGN)
-def awgn(y, snr):
+def add_awgn(y, snr):
     snr = 10 ** (snr / 10.0)
     xpower = np.sum(y ** 2) / len(y)
     npower = xpower / snr
     return np.random.randn(len(y)) * np.sqrt(npower) + y
 
-noise_bpsk = awgn(bpsk, 5)
+def filter_signals(input_signal, fs, carrier):
+    b11, a11 = signal.ellip(5, 0.5, 60, [2000 * 2 / fs, 6000 * 2 / fs], btype='bandpass', output='ba')
+    b12, a12 = signal.ellip(5, 0.5, 60, 2000 * 2 / fs, btype='lowpass', output='ba')
+    bandpass_out = signal.filtfilt(b11, a11, input_signal)
+    coherent_demod = bandpass_out * (carrier * 2)
+    lowpass_out = signal.filtfilt(b12, a12, coherent_demod)
+    return lowpass_out
 
-# Filtering
-b11, a11 = signal.ellip(5, 0.5, 60, [2000 * 2 / fs, 6000 * 2 / fs], btype='bandpass', analog=False, output='ba')
-b12, a12 = signal.ellip(5, 0.5, 60, 2000 * 2 / fs, btype='lowpass', analog=False, output='ba')
-bandpass_out = signal.filtfilt(b11, a11, noise_bpsk)
-coherent_demod = bandpass_out * (coherent_carrier * 2)
-lowpass_out = signal.filtfilt(b12, a12, coherent_demod)
+def sample_and_decide(lowpass_out, size, sigma2):
+    detection_bpsk = np.zeros(len(lowpass_out), dtype=np.float32)
+    flag = np.zeros(size, dtype=np.float32)
+    for i in range(size):
+        x = lowpass_out[i * 100: (i + 1) * 100]
+        llr = np.sum(2 * x / sigma2)
+        flag[i] = 1 if llr > 0 else 0
+        detection_bpsk[i * 100: (i + 1) * 100] = flag[i]
+    return detection_bpsk
 
-# Sampling and decision making
-detection_bpsk = np.zeros(len(t), dtype=np.float32)
-flag = np.zeros(size, dtype=np.float32)
-for i in range(size):
-    tempF = np.sum(lowpass_out[i * 100: (i + 1) * 100])
-    flag[i] = 1 if tempF > 0 else 0
-    detection_bpsk[i * 100: (i + 1) * 100] = flag[i]
+# Parameters
+size = 10
+sampling_t = 0.01
+fc = 4000
+fs = 40 * fc
+sigma2 = 1.0
+
+# Main execution
+t, m = generate_binary_sequence(size, sampling_t)
+bpsk, coherent_carrier = modulate_signal(t, m, fc, fs)
+noise_bpsk = add_awgn(bpsk, 5)  # SNR = 5 dB for example
+lowpass_out = filter_signals(noise_bpsk, fs, coherent_carrier)
+detection_bpsk = sample_and_decide(lowpass_out, size, sigma2)
 
 # Plot setup
 fig, axs = plt.subplots(3, 1, figsize=(10, 15))
